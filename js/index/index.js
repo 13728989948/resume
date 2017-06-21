c11c538ba172a6a6b924609dbe3fc9ed7e71fe33
git <git>
2017-06-21 15:59:43 +0800

    Signed-off-by: git <git>

--- js/index/index.js
+++ js/index/index.js
@@ -84,6 +84,16 @@ function getJnTwConHtml() {
 	html += "                       <td class=\"sinJn cell\" valign=\"top\"><table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">" + "\n";
 	html += "                         <tbody>" + "\n";
 	html += "                          <tr>" + "\n";
+	html += "                           <td class=\"skill\">Radis</td>" + "\n";
+	html += "                           <td valign=\"top\"><span class=\"skbg\"><span class=\"skco\">一般</span></span></td>" + "\n";
+	html += "                          </tr>" + "\n";
+	html += "                         </tbody>" + "\n";
+	html += "                        </table></td>" + "\n";
+	html += "                      </tr>" + "\n";
+	html += "                      <tr>" + "\n";
+	html += "                       <td class=\"sinJn cell\" valign=\"top\"><table cellspacing=\"0\" cellpadding=\"0\" border=\"0\">" + "\n";
+	html += "                         <tbody>" + "\n";
+	html += "                          <tr>" + "\n";
 	html += "                           <td class=\"skill\">Varnish</td>" + "\n";
 	html += "                           <td valign=\"top\"><span class=\"skbg\"><span class=\"skco\">一般</span></span></td>" + "\n";
 	html += "                          </tr>" + "\n";
